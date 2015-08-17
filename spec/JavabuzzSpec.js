c0bf7e47fa4e4e09c9226c0114ac2465248e5ced
Retesh <retesh.bajaj@gmail.com>
2015-08-17 11:46:28 +0100

    test1 passing

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -1,2 +1,15 @@
-describe('', function() {});
+describe('Javabuzz', function() {
+
+  var javabuzz
+
+  describe ('knows what a number is', function () {
+
+    it('is divisible by 3', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+    });
+
+  });
+
+});
 
