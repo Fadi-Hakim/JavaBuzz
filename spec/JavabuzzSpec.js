ec0cf2f18c446cf32c8f82e3bec77936617bcd6b
Retesh <retesh.bajaj@gmail.com>
2015-08-17 15:18:21 +0100

    jabuzz complete

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -10,7 +10,7 @@ describe('Javabuzz', function() {
 
     it('is divisible by 3', function() {
       // javabuzz = new Javabuzz();
-      expect(javabuzz.isDivisibleBy(3, 3)).toBe(true);
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
 
     it('is divisible by 5', function() {
@@ -35,6 +35,26 @@ describe('Javabuzz', function() {
 
     it('divisible by 15', function() {
       expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
+    });
+
+  });
+
+  describe('when playing, says', function() {
+
+    it('"Java" when a number is divisivle by 3', function() {
+      expect(javabuzz.says(3)).toEqual("Java");
+    });
+
+    it('"Buzz" when a number is divisivle by 5', function() {
+      expect(javabuzz.says(5)).toEqual("Buzz");
+    });
+
+    it('"JavaBuzz" when a number is divisivle by 15', function() {
+      expect(javabuzz.says(15)).toEqual("JavaBuzz");
+    });
+
+    it('returns number when not divisible by 3 and 5', function() {
+      expect(javabuzz.says(7)).toEqual(7);
     });
 
   });
